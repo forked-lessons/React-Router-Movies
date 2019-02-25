5f322af2e02ee67ac9e7ab609630a262b01d8a05
Daniel <danielstarling4@gmail.com>
2019-02-25 16:40:19 -0700

    Stretch: refactor to MovieCard

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import MovieCard from "./MovieCard";
 
 export default class Movie extends Component {
   constructor(props) {
@@ -45,7 +46,8 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
+        <MovieCard movie={this.state.movie} />
+        {/* <div className="movie-card">
           <h2>{title}</h2>
           <div className="movie-director">
             Director: <em>{director}</em>
@@ -60,7 +62,7 @@ export default class Movie extends Component {
               {star}
             </div>
           ))}
-        </div>
+        </div> */}
         <div className="save-button">Save</div>
       </div>
     );
