6a746b97bc112731eb813830f9f6e3c6d2004b67
Dstar4 <danielstarling4@gmail.com>
2019-02-25 13:13:16 -0700

    inital commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
-
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 export default class App extends Component {
   constructor() {
     super();
@@ -22,7 +22,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route path="/" exact component={MovieList} />
+        <Route path="/movies/:id" component={Movie} />
       </div>
     );
   }
